f9be9c4a09c5d424669b88dd1a5385d730ed8ca1
Jyoti kumar thakur <vikashthakur701@gmail.com>
2024-09-05 23:47:58 +0530

    Your commit message

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.js'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { approute } from './App.jsx'
